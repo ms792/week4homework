ece1c21f8c64d927c500578d02ca3bec703667c7
Monica Sangam <ms792@njit.edu>
2017-10-04 18:48:46 -0400

    added index changed -ifelse

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ echo "The value of \$date: ".$date."<br>";
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
+if($date > $tar) {
+	echo "The future<br>";
+} else if($date < $tar)  {
+	echo "The past<br>";
+}
+else {
+	echo "oops<br>";
+}
+
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
