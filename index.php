98d585d448fb798f97cdd2fc93e26b84e14d8e82
Monica Sangam <ms792@njit.edu>
2017-10-04 20:53:11 -0400

    added index changed - words array

--- index.php
+++ index.php
@@ -49,6 +49,8 @@ echo "ascii_value : $ascii_value <br>";
 $last_two_char = substr($date, strlen($date)-2, strlen($date));
 echo "last_two_char : $last_two_char <br>";
 
+list($month, $day, $year) = split('[/.-]', $date);
+echo "words : $month $day $year <br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
